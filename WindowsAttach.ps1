0c6b397632c9531843ce5a963a05b64e9436f8a1
Nathan Mahoney <nathan17x@gmail.com>
2025-06-21 20:00:44 -0400

    icon

--- WindowsAttach.ps1
+++ WindowsAttach.ps1
@@ -1,5 +1,5 @@
 $userProfilePath = [System.Environment]::GetFolderPath('UserProfile')
-$shortcutPath = Join-Path -Path $userProfilePath -ChildPath "Dev Container.lnk"
+$shortcutPath = Join-Path -Path $userProfilePath -ChildPath "nm-toolbox.lnk"
 
 $shell = New-Object -ComObject WScript.Shell
 $shortcut = $shell.CreateShortcut($shortcutPath)
@@ -7,8 +7,8 @@ $shortcut = $shell.CreateShortcut($shortcutPath)
 $shortcut.TargetPath = "wt.exe"
 $shortcut.Arguments = "docker exec -it nm-toolbox zsh"
 $shortcut.Description = "Launches a zsh shell in the nm-toolbox container."
-$shortcut.IconLocation = "C:\Windows\System32\imageres.dll, 2"
+$shortcut.IconLocation = "wt.exe"
 $shortcut.Save()
 
-Write-Host "Success! Shortcut 'Dev Container.lnk' has been created in your user directory ($userProfilePath)."
+Write-Host "Success! Shortcut 'nm-toolbox.lnk' has been created in your user directory ($userProfilePath)."
 Write-Host "You can now find it there, right-click it, and choose 'Pin to taskbar'."
